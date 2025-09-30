eb36e65f95c4c52f44ab46e88d5a90ec8d47afae
recreators-designs <recreaters.services@gmail.com>
2025-09-30 12:32:51 +0530

    change mail setting

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,14 +2,16 @@ import nodemailer from 'nodemailer'
 
 const sendEmail = async (mailoption) => {
     const transporter = nodemailer.createTransport({
-        service : 'gmail',
+        host: "mail.bookmyflowers.shop",
+        port: 465,
+        secure: true,
         auth : {
             user : process.env.ADMIN_EMAIL,
             pass : process.env.ADMIN_PASSWORD
             }
             });
         const mailOptions = {
-            from: process.env.ADMIN_EMAIL,
+            from: '"Book My Flowers' + process.env.ADMIN_EMAIL,
             to: mailoption.to,
             subject: mailoption.subject,
             html: mailoption.text,
