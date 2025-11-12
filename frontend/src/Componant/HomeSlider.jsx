29e6c7ebd9cd45b4c0d7afb235e3670a8bec25a8
recreators-designs <recreaters.services@gmail.com>
2025-11-12 10:28:42 +0530

    ui changes

--- frontend/src/Componant/HomeSlider.jsx
+++ frontend/src/Componant/HomeSlider.jsx
@@ -90,10 +90,10 @@ const HomeSlider = () => {
                     effect='fade'
                     loop={banners.length > 1}
                     slidesPerView={1}
-                    navigation={{
-                        nextEl: '.custom-next',
-                        prevEl: '.custom-prev',
-                    }}
+                    // navigation={{
+                    //     nextEl: '.custom-next',
+                    //     prevEl: '.custom-prev',
+                    // }}
                 >
                     {banners.length > 0 ? (
                         banners.map((banner, index) => (
@@ -139,7 +139,7 @@ const HomeSlider = () => {
                 </Swiper>
 
                 {/* Custom navigation arrows */}
-                <div className="custom-navigation">
+                {/* <div className="custom-navigation">
                     <div className="custom-prev">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path d="M15 18L9 12L15 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -150,7 +150,7 @@ const HomeSlider = () => {
                             <path d="M9 18L15 12L9 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </div>
-                </div>
+                </div> */}
             </div>
 
             {/* Error message */}
